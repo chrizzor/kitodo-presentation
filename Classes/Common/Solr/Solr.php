f8b392389b4918fea4da230fef366d4b87791f1e
Christopher Timm <timm@effective-webwork.de>
2024-05-17 16:09:06 +0200

    Use solarium 6.x

--- Classes/Common/Solr/Solr.php
+++ Classes/Common/Solr/Solr.php
@@ -586,12 +586,12 @@ class Solr implements LoggerAwareInterface
             // Todo: When updating to TYPO3 >=10.x and Solarium >=6.x
             // the timeout must be set with the adapter instead of the
             // endpoint (see below).
-            // $adapter->setTimeout($this->config['timeout']);
+        $adapter->setTimeout($this->config['timeout']);
         // Configure event dispatcher.
             // Todo: When updating to TYPO3 >=10.x and Solarium >=6.x
             // we have to provide an PSR-14 Event Dispatcher instead of
             // "null".
-            // $eventDispatcher = GeneralUtility::makeInstance(\TYPO3\CMS\Core\EventDispatcher\EventDispatcher::class);
+        $eventDispatcher = GeneralUtility::makeInstance(\TYPO3\CMS\Core\EventDispatcher\EventDispatcher::class);
         // Configure endpoint.
         $config = [
             'endpoint' => [
@@ -602,18 +602,18 @@ class Solr implements LoggerAwareInterface
                     'path' => '/' . $this->config['path'],
                     'core' => $core,
                     'username' => $this->config['username'],
-                    'password' => $this->config['password'],
-                    'timeout' => $this->config['timeout'] // Remove when upgrading to Solarium 6.x
+                    'password' => $this->config['password']
+//                    'timeout' => $this->config['timeout'] // Remove when upgrading to Solarium 6.x
                 ]
             ]
         ];
         // Instantiate Solarium\Client class.
-        $this->service = GeneralUtility::makeInstance(Client::class, $config);
-        $this->service->setAdapter($adapter);
+//        $this->service = GeneralUtility::makeInstance(Client::class, $config);
+//        $this->service->setAdapter($adapter);
             // Todo: When updating to TYPO3 >=10.x and Solarium >=6.x
             // $adapter and $eventDispatcher are mandatory arguments
             // of the \Solarium\Client constructor.
-            // $this->service = GeneralUtility::makeInstance(\Solarium\Client::class, $adapter, $eventDispatcher, $config);
+        $this->service = GeneralUtility::makeInstance(\Solarium\Client::class, $adapter, $eventDispatcher, $config);
         // Check if connection is established.
         $query = $this->service->createCoreAdmin();
         $action = $query->createStatus();
